39e2411e36d57d6b41c55b501db2322baebcc503
Hayat-ui <hayatkhalid045@gmail.com>
2024-10-15 17:39:19 +0500

    Update Time.h

--- Time.h
+++ Time.h
@@ -7,30 +7,17 @@ class Time
 public:
 	Time();
 	Time(int h, int m, int s) :Time();
-	
-	void setHour(int h);
-	
-	void setMinute(int m);
-	
-	void setSecond(int s);
-	
-	void setTime(int h, int m, int s);
-	
-	int getHour()const;
-	
+	void setHour(int h);	
+	void setMinute(int m);	
+	void setSecond(int s);	
+	void setTime(int h, int m, int s);	
+	int getHour()const;	
 	int getMinute()const;
-
 	int getSecond()const;
-
-		void printTwentyFourHourFormat()const;
-
-		void printTwelveHourFormat()const;
-
-		void incHour(int inc = 1);
-
-		void incMin(int inc = 1);
-
-		void incSec(int inc = 1);
-	
+	void printTwentyFourHourFormat()const;
+	void printTwelveHourFormat()const;
+	void incHour(int inc = 1);
+	void incMin(int inc = 1);
+	void incSec(int inc = 1);	
 };
 
